4512a71bc5fa432b34d0b8ee844d0b78cbb7b11d
Ayala Rozenberg <ayalad02120@gmail.com>
2026-06-08 10:47:59 +0300

    kj

--- nextjs-dashboard/app/ui/fonts.ts
+++
@@ -1,8 +0,0 @@
-import { Inter,Lustria } from 'next/font/google';
- 
-export const inter = Inter({ subsets: ['latin'] });
-
-export const lusitana  = Lustria({
-    subsets: ['latin'],
-    weight: '400'
-});
